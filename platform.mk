6fdc2f21bba754125503e69a0f578c8564307205
Felix <none@none.none>
2018-10-18 06:02:33 +0200

    Disable dm-verity

--- platform.mk
+++ platform.mk
@@ -170,5 +170,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.wifi.addr_path=/sys/devices/platform/soc/soc:bcmdhd_wlan/macaddr
 
 # setup dm-verity configs.
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7464900.sdhci/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
+# PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7464900.sdhci/by-name/system
+# $(call inherit-product, build/target/product/verity.mk)
